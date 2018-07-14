48300fe8626b60687bf1efdaebaf8cb19b021206
Christina Ng <christinasy317@gmail.com>
2018-07-14 22:16:48 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,23 +1,27 @@
 def my_hash
   # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
-  my_hash = { "my_name" => "Christina"
-              "my_best_friend" => "Katie"}
-  
+  my_hash = { "my name" => "Christina",
+              "my best friend" => "Katie"}
+
 end
 
 
 def shipping_manifest
   # set a variable called `the_manifest`, equal to a hash
-  # fill that hash with key/value pairs that describe the following information: 
-  # We have 5 whale bone corsets, 2 porcelain vases and 3 oil paintings 
+  # fill that hash with key/value pairs that describe the following information:
+  # We have 5 whale bone corsets, 2 porcelain vases and 3 oil paintings
 
+  the_manifest = { "whale bone corsets" => 5,
+                   "porcelain vases" => 2,
+                   "oil paintings" => 3
+          }
 
 end
 
 def retrieval
   shipping_manifest = {
-                  "whale bone corsets" => 5, 
-                  "porcelain vases" => 2, 
+                  "whale bone corsets" => 5,
+                  "porcelain vases" => 2,
                   "oil paintings" => 3
                  }
 
@@ -27,8 +31,8 @@ end
 
 def adding
   shipping_manifest = {
-                  "whale bone corsets" => 5, 
-                  "porcelain vases" => 2, 
+                  "whale bone corsets" => 5,
+                  "porcelain vases" => 2,
                   "oil paintings" => 3
                  }
   # add 2 muskets to the shipping_manifest hash below
